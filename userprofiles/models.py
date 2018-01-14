8496e3d9f31f26624bb25733c42293d20b14b0cb
Stan <smisiurev@gmail.com>
2018-01-13 22:57:54 -0500

    settings.AUTH_USER_MODEL instead of hardcoded model

--- userprofiles/models.py
+++ userprofiles/models.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 
 from django.db import models
-from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
+from django.conf import settings
 
 
 GENDER =(('man','Man'),('woman','Woman'))
 
 
 class UserProfile(models.Model):  
-    user = models.OneToOneField(User, null=True, related_name="profile", verbose_name=_(u'User'))
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, null=True, related_name="profile", verbose_name=_(u'User'))
     phone = models.PositiveIntegerField(null=True, blank=True)
     gender = models.CharField(max_length=40, blank=True, verbose_name=_(u'Gender'), choices=GENDER) 
     avatar = models.ImageField(upload_to='userprofiles/avatars', null=True, blank=True, verbose_name=_(u"Avatar"))
